c70712470bd7bf2ccebced4c9dc85fb4314fbd64
geekworm <46470628+geekworm-com@users.noreply.github.com>
2021-06-25 22:05:59 +0800

    Add files via upload

---
+++ ubuntu-mate.sh
@@ -0,0 +1,124 @@
+#IMPORTANT! This script is only for the x-c1
+#x-c1 Powering on /reboot /full shutdown through hardware
+#!/bin/bash
+
+AUTO_RUN=/etc/profile.d/naspi.sh
+# USER_RUN_FILE=/etc/bash.bashrc
+USER_RUN_FILE=${AUTO_RUN}
+echo '#!/bin/bash
+
+SHUTDOWN=4
+REBOOTPULSEMINIMUM=200
+REBOOTPULSEMAXIMUM=600
+echo "$SHUTDOWN" > /sys/class/gpio/export
+echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
+BOOT=17
+echo "$BOOT" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio$BOOT/direction
+echo "1" > /sys/class/gpio/gpio$BOOT/value
+
+echo "Your device are shutting down..."
+
+while [ 1 ]; do
+  shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
+  if [ $shutdownSignal = 0 ]; then
+    /bin/sleep 0.2
+  else
+    pulseStart=$(date +%s%N | cut -b1-13)
+    while [ $shutdownSignal = 1 ]; do
+      /bin/sleep 0.02
+      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
+        echo "Your device are shutting down", SHUTDOWN, ", halting Rpi ..."
+        sudo poweroff
+        exit
+      fi
+      shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
+    done
+    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
+      echo "Your device are rebooting", SHUTDOWN, ", recycling Rpi ..."
+      sudo reboot
+      exit
+    fi
+  fi
+done' > /etc/x-c1-pwr.sh
+sudo chmod +x /etc/x-c1-pwr.sh
+#sudo sed -i '$ i /etc/x-c1-pwr.sh &' ${USER_RUN_FILE}
+
+#x-c1 full shutdown through python
+
+#x-c1 full shutdown through shell
+#!/bin/bash
+echo '
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+GPIO_PORT    = 20
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PORT, GPIO.OUT)
+print "Shutdown in 5 seconds"
+time.sleep(5)
+GPIO.output(GPIO_PORT, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(GPIO_PORT, GPIO.LOW)
+GPIO.cleanup()
+' >> /etc/x-c1-softsd.py
+sudo echo "alias xoff='python /etc/x-c1-softsd.py'" >> ${AUTO_RUN}
+
+echo '#!/bin/bash
+
+BUTTON=27
+
+echo "$BUTTON" > /sys/class/gpio/export;
+echo "out" > /sys/class/gpio/gpio$BUTTON/direction
+echo "1" > /sys/class/gpio/gpio$BUTTON/value
+
+SLEEP=${1:-4}
+
+re='^[0-9\.]+$'
+if ! [[ $SLEEP =~ $re ]] ; then
+   echo "error: sleep time not a number" >&2; exit 1
+fi
+
+echo "Your device will shutting down in 4 seconds..."
+/bin/sleep $SLEEP
+
+echo "0" > /sys/class/gpio/gpio$BUTTON/value
+' > /usr/local/bin/x-c1-softsd.sh
+sudo chmod +x /usr/local/bin/x-c1-softsd.sh
+# sudo echo "alias xoff='sudo x-c1-softsd.sh'" >> ${AUTO_RUN}
+
+# create pigpiog service - begin
+service_name="/lib/systemd/system/pigpiod.service"
+# Create service file on system.
+if [ -e $service_name ]; then
+	sudo rm $service_name -f
+fi
+
+sudo echo '[Unit]
+Description=Daemon required to control GPIO pins via pigpio
+[Service]
+ExecStart=/usr/local/bin/pigpiod -l
+ExecStop=/bin/systemctl kill pigpiod
+Type=forking
+[Install]
+WantedBy=multi-user.target
+' >> ${service_name}
+
+# create pigpiog service - begin
+
+sudo systemctl enable pigpiod
+
+CUR_DIR=$(pwd)
+sudo echo "python ${CUR_DIR}/fan.py&"  >> ${AUTO_RUN}
+
+sudo pigpiod
+python ${CUR_DIR}/fan.py&
+
+echo "The installation is complete."
+echo "Please run 'sudo reboot' to reboot the device."
+echo "NOTE:"
+echo "1. DON'T modify the name fold: $(basename ${CUR_DIR}), or the PWM fan will not work after reboot."
+echo "2. fan.py is python file to control fan speed according temperature of CPU, you can modify it according your needs."
+echo "3. PWM fan needs a PWM signal to start working. If fan doesn't work in third-party OS afer reboot only remove the YELLOW wire to let the fan run immediately or contact us: info@geekworm.com."
